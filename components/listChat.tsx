77dc34cb8a465b37409bee65ab50362b477bd4d6
Khang080704 <pgkhangt1@gmail.com>
2025-09-02 09:44:28 +0700

    fix UI bug

--- components/listChat.tsx
+++ components/listChat.tsx
@@ -2,9 +2,11 @@
 import { chatListStore } from "@/app/store/list";
 import ChatTitle from "./ChatTitle";
 import { useEffect } from "react";
+import { useSidebar } from "./ui/sidebar";
 
 export default  function ListChat({list}: {list: any[]}) {
     const {setList, listChat} = chatListStore();
+    const {state} = useSidebar()
 
     useEffect(() => {
         setList(list);
@@ -12,7 +14,9 @@ export default  function ListChat({list}: {list: any[]}) {
 
     return (
         <div className="">
-            <h2 className="text-gray-400 text-xl my-2 px-3">Chats</h2>
+            {state !== 'collapsed' && (
+                <h2 className="text-gray-400 text-xl my-2 px-3">Chats</h2>
+            )}
             <ul>
                 {listChat.map((chat) => (
                     <div key={`${chat.sessionId}`} className="cursor-pointer">
