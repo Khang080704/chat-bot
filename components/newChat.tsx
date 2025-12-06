9b0a59f39fe6858fea9be3aa351c7e890d683f37
Khang080704 <pgkhangt1@gmail.com>
2025-12-06 14:34:49 +0700

    update: update next's version from 15.4.6 to 15.4.8 to prevent next security bug. Update multiple line in user's prompt

--- components/newChat.tsx
+++ components/newChat.tsx
@@ -13,13 +13,15 @@ export default function NewChat() {
     };
 
     return state === "expanded" && !isMobile ? (
-        <div
-            className="flex cursor-pointer items-center gap-2 text-md hover:bg-gray-200 p-3 rounded-2xl"
-            onClick={handleNewChat}
-        >
-            <Edit />
-            New Chat
-        </div>
+        <>
+            <div
+                className={`flex cursor-pointer items-center gap-2 text-md hover:bg-gray-200 p-3 rounded-2xl`}
+                onClick={handleNewChat}
+            >
+                <Edit />
+                New Chat
+            </div>
+        </>
     ) : state === "expanded" && isMobile ? (
         <div
             className="flex cursor-pointer items-center gap-2 text-md hover:bg-gray-200 p-3 rounded-2xl"
@@ -29,11 +31,13 @@ export default function NewChat() {
             New Chat
         </div>
     ) : (
-        <div
-            className="hover:bg-gray-200 rounded-2xl flex items-center justify-center p-1"
-            onClick={handleNewChat}
-        >
-            <Edit />
-        </div>
+        <>
+            <div
+                className="hover:bg-gray-200 rounded-2xl flex items-center justify-center p-1 mb-2"
+                onClick={handleNewChat}
+            >
+                <Edit />
+            </div>
+        </>
     );
 }
